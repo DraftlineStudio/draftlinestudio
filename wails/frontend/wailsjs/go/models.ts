03602f7d850450635903a838b6b5721fc4101cd3
JL Griffin <1276255+griffinwebnet@users.noreply.github.com>
2026-03-17 18:04:21 -0500

    Improve the generated Wails bindings

--- wails/frontend/wailsjs/go/models.ts
+++ wails/frontend/wailsjs/go/models.ts
@@ -233,6 +233,61 @@ export namespace main {
 	    }
 	}
 	
+	export class RecentProjectStats {
+	    books?: number;
+	    chapters: number;
+	    words: number;
+	
+	    static createFrom(source: any = {}) {
+	        return new RecentProjectStats(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.books = source["books"];
+	        this.chapters = source["chapters"];
+	        this.words = source["words"];
+	    }
+	}
+	export class RecentProject {
+	    type: string;
+	    path: string;
+	    name: string;
+	    lastOpened: string;
+	    stats: RecentProjectStats;
+	
+	    static createFrom(source: any = {}) {
+	        return new RecentProject(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.type = source["type"];
+	        this.path = source["path"];
+	        this.name = source["name"];
+	        this.lastOpened = source["lastOpened"];
+	        this.stats = this.convertValues(source["stats"], RecentProjectStats);
+	    }
+	
+		convertValues(a: any, classs: any, asMap: boolean = false): any {
+		    if (!a) {
+		        return a;
+		    }
+		    if (a.slice && a.map) {
+		        return (a as any[]).map(elem => this.convertValues(elem, classs));
+		    } else if ("object" === typeof a) {
+		        if (asMap) {
+		            for (const key of Object.keys(a)) {
+		                a[key] = new classs(a[key]);
+		            }
+		            return a;
+		        }
+		        return new classs(a);
+		    }
+		    return a;
+		}
+	}
+	
 	export class SaveResult {
 	    success: boolean;
 	    file_path: string;
